47adbb825481ad3e65886f8ea73faaff46947df0
Anton <anton@adc.sh>
2018-10-07 04:13:16 +0300

    b

--- build/index.js
+++ build/index.js
@@ -1,30 +1,38 @@
-const { debuglog } = require('util');
-
-const LOG = debuglog('forkfeed')
-
 /**
- * Passes answer values to a child process on data.
- * @param {Config} [config] Options for the program.
- * @param {boolean} [config.shouldRun=true] A boolean option. Default `true`.
- * @param {string} config.text A text to return.
+ * Write data to the `writable` when data from the `readable` matches the regexp.
+ * @param {Readable} readable A readable stream to detect data on.
+ * @param {Writable} stdin A writable stream to pass answers to.
+ * @param {[RegExp, string][]} inputs A serial collection of answers. Each answer will be ended with a `\n` character.
+ * @param {Writable} [log] A stream to which to write both data from readable, and the passed answer.
  */
-               async function forkfeed(config = {}) {
-  const {
-    shouldRun = true,
-    text,
-  } = config
-  if (!shouldRun) return
-  LOG('forkfeed called with %s', text)
-  return text
+const forkFeed = (readable, stdin, inputs = [], log) => {
+  if (log) readable.on('data', d => log.write(d))
+
+  let [a, ...rest] = inputs
+  if (!a) return
+
+  const handler = (d) => {
+    const [regexp, answer] = a
+    if (!regexp.test(d)) return
+
+    const an = `${answer}\n`
+    if (log) log.write(an)
+
+    stdin.write(an)
+    ;([a, ...rest] = rest)
+    if (!a) readable.removeListener('data', handler)
+  }
+  readable.on('data', handler)
 }
+
+module.exports=forkFeed
 
 /* documentary types/index.xml */
 /**
- * @typedef {Object} Config Options for the program.
- * @prop {boolean} [shouldRun=true] A boolean option. Default `true`.
- * @prop {string} text A text to return.
+ * @typedef {import('stream').Writable} Writable
+ * @typedef {import('stream').Readable} Readable
+ *
+
  */
 
-
-module.exports = forkfeed
 //# sourceMappingURL=index.js.map
